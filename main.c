86fae07b41af4fd04e4b42cd48c46c2e2e9a781e
Jayant Chauhan <105379510+Jayant7579@users.noreply.github.com>
2023-08-04 18:54:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,48 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int get_1st_weekday(int year){
+int main() {
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
 
-  int d;
-  d = (((year - 1) * 365) + ((year - 1) / 4) - ((year - 1) / 100) + ((year) / 400) + 1) % 7;
-  return d;
-}
+    struct tm firstDay = { .tm_year = year - 1900, .tm_mon = 0, .tm_mday = 1 };
+    mktime(&firstDay);
 
-int main()
-{
-   system("Color 3F");
-   int year,month,day,daysInMonth,weekDay=0,startingDay;
-   printf("\nEnter your desired year:");
-   scanf("%d",&year);
+    printf("\nCALENDAR %d\n", year);
 
-   char *months[]={"January","February","March","April","May","June","July","August","September","October","November","December"};
-   int monthDay[]={31,28,31,30,31,30,31,31,30,31,30,31};
+    for (int month = 0; month < 12; month++) {
+        struct tm currentDay = firstDay;
 
-   if((year%4==0&&year%100!=0)||year%400==0)
-       monthDay[1]=29;
+        currentDay.tm_mon = month;
+        mktime(&currentDay);
 
-   startingDay=get_1st_weekday(year);
+        printf("\n%s\n", asctime(&currentDay));
+        printf("Sun Mon Tue Wed Thu Fri Sat\n");
 
-   for(month=0;month<12;month++){
+        int weekday = currentDay.tm_wday;
 
-      daysInMonth=monthDay[month];
-      printf("\n\n---------------%s-------------------\n",months[month]);
-      printf("\n  Sun  Mon  Tue  Wed  Thurs  Fri  Sat\n");
-
-      for(weekDay=0;weekDay<startingDay;weekDay++)
-        printf("     ");
-
-      for(day=1;day<=daysInMonth;day++){
-        printf("%5d",day);
-
-        if(++weekDay>6){
-            printf("\n");
-            weekDay=0;
+        for (int i = 0; i < weekday; i++) {
+            printf("    ");
         }
-        startingDay=weekDay;
-      }
 
-   }
+        int daysInMonth = 31 - (month == 1) - (month == 3 || month == 5 || month == 8 || month == 10);
 
+        for (int day = 1; day <= daysInMonth; day++) {
+            printf("%3d ", day);
 
+            if (++weekday > 6) {
+                weekday = 0;
+                printf("\n");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
 }
